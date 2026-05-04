edb1ffbb1d312b3c11b1c09588fd40ed93b5a369
CodeGiorgino <andreagiorgino1@gmail.com>
2026-05-04 19:02:33 +0200

    code cleanup

--- include/parser.hxx
+++ include/parser.hxx
@@ -5,11 +5,20 @@
 
 class parser final {
     public:
-        explicit parser(std::string_view filePath);
+        parser(void) = delete;
+        explicit parser(std::filesystem::path filePath);
+
+        parser(const parser&) = delete;
+        auto operator =(const parser&) = delete;
+
+        parser(parser&&) = delete;
+        auto operator =(parser&&) = delete;
+
+        ~parser(void) noexcept = default;
 
     public:
         auto lines(void) noexcept -> std::generator<std::string>;
 
     private:
-        std::filesystem::path filePath {};
+        std::filesystem::path _filePath {};
 };
