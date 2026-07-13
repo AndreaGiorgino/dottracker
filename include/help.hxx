d964f60204831c81a7cc2e8c27dad59044c76cda
giorgi.page <andreagiorgino1@gmail.com>
2026-07-13 12:36:17 +0200

    env variables implemented for configuration

--- include/help.hxx
+++ include/help.hxx
@@ -5,55 +5,65 @@
 
 static constexpr auto helpMessage {
     ANSI_BOLD
-    "NAME                                                                          \n"
+    "NAME                                                                       \n"
     ANSI_RESET
-    "    dottracker - dotfiles tracker                                             \n"
-    "                                                                              \n"
+    "    dottracker - dotfiles tracker                                          \n"
+    "                                                                           \n"
     ANSI_BOLD
-    "SYNOPSIS                                                                      \n"
+    "SYNOPSIS                                                                   \n"
     ANSI_RESET
-    "    dottracker [-v | --version] [-h | --help] <command> [<args>]              \n"
-    "                                                                              \n"
+    "    dottracker [-v | --version] [-h | --help] <command> [<args>]           \n"
+    "                                                                           \n"
     ANSI_BOLD
-    "DESCRIPTION                                                                   \n"
+    "DESCRIPTION                                                                \n"
     ANSI_RESET
-    "    dottracker is a tracking program for UNIX dotfiles and configurations.    \n"
-    "                                                                              \n"
+    "    dottracker is a tracking program for UNIX dotfiles and configurations. \n"
+    "                                                                           \n"
     ANSI_BOLD
-    "OPTIONS                                                                       \n"
+    "ENVIROMENT                                                                 \n"
     ANSI_RESET
-    "    -v, --version                                                             \n"
-    "        Prints the program version and exit.                                  \n"
-    "                                                                              \n"
-    "    -h, --help                                                                \n"
-    "        Prints the program help message and exit.                             \n"
-    "                                                                              \n"
+    "    DOTTRACKER_CONFIG                                                      \n"
+    "        Defaults to '$HOME/.config/dottracker/files.conf', represents the  \n"
+    "        location of the configuration file.                                \n"
+    "                                                                           \n"
+    "    DOTTRACKER_ARCHIVE                                                     \n"
+    "        Required, represents the location of the archived files.           \n"
+    "                                                                           \n"
     ANSI_BOLD
-    "COMMANDS                                                                      \n"
+    "OPTIONS                                                                    \n"
     ANSI_RESET
-    "    update <target> [<-s | --source> <file_path>]                             \n"
-    "        Update files in target:                                               \n"
-    "            - local: repository files overwrites local files.                 \n"
-    "            - repo:  local files overwrites repository files.                 \n"
-    "                                                                              \n"
+    "    -v, --version                                                          \n"
+    "        Prints the program version and exit.                               \n"
+    "                                                                           \n"
+    "    -h, --help                                                             \n"
+    "        Prints the program help message and exit.                          \n"
+    "                                                                           \n"
     ANSI_BOLD
-    "        Options:                                                              \n"
+    "COMMANDS                                                                   \n"
     ANSI_RESET
-    "            -s, --source <file_path>                                          \n"
-    "                If specified, use the file provided for the update, otherwise \n"
-    "                '$HOME/.config/dottracker/files.conf' is used.                \n"
-    "                                                                              \n"
-    "    diff [<-s | --source> <file_path>]                                        \n"
-    "        Print a diff of the tracked files:                                    \n"
-    "            - OK: local and repository file are in sync.                      \n"
-    "            - A:  local file has been added                                   \n"
-    "            - M:  local file has been modified                                \n"
-    "            - D:  local file has been deleted                                 \n"
-    "                                                                              \n"
+    "    diff [<-s | --source> <file_path>]                                     \n"
+    "        Print a diff of the tracked files:                                 \n"
+    "            - OK: local and archive file are in sync.                      \n"
+    "            - A:  local file has been added                                \n"
+    "            - M:  local file has been modified                             \n"
+    "            - D:  local file has been deleted                              \n"
+    "                                                                           \n"
     ANSI_BOLD
-    "        Options:                                                              \n"
+    "        Options:                                                           \n"
     ANSI_RESET
-    "            -s, --source <file_path>                                          \n"
-    "                If specified, use the file provided for the diff, otherwise   \n"
-    "                '$HOME/.config/dottracker/files.conf' is used."
+    "            -s, --source <file_path>                                       \n"
+    "                If specified, use the file provided for the diff,          \n"
+    "                otherwise DOTTRACKER_CONFIG is used.                       \n"
+    "                                                                           \n"
+    "    update <target> [<-s | --source> <file_path>]                          \n"
+    "        Update files in target:                                            \n"
+    "            - local:   archive files overwrites local files.               \n"
+    "            - archive: local files overwrites archive files.               \n"
+    "                                                                           \n"
+    ANSI_BOLD
+    "        Options:                                                           \n"
+    ANSI_RESET
+    "            -s, --source <file_path>                                       \n"
+    "                If specified, use the file provided for the update,        \n"
+    "                otherwise DOTTRACKER_CONFIG is used.                       \n"
 };
