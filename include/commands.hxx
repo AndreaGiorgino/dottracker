b6d6e56f7e1f6ba040c350797a38e1318a6b125f
giorgi.page <andreagiorgino1@gmail.com>
2026-07-13 12:35:51 +0200

    source files reorganised

--- include/commands.hxx
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "enviroment.hxx"
-
-namespace commands {
-    auto update(const enviroment& env) -> void;
-    auto diff(const enviroment& env) -> void;
-} // namespace commands
